82ea6cdcdaed1b79064a8528534a9f70e6397f1b
M1057071 <M1057071@A2ML36455.mindtree.com>
2020-04-13 19:21:49 +0530

    zuul commit

---
+++ netflix-zuul-api-gateway-server/src/test/java/com/sample/microservices/go/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+/*package com.sample.microservices.go.netflixzuulapigatewayserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
